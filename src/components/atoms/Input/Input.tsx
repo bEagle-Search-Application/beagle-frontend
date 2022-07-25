4d10f19b029ed7a71c08ef2cc488d2da4467b2cb
Darwin_N19 <darwinccn25@gmail.com>
2022-07-25 17:35:33 -0500

    wip: input component

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -8,16 +8,18 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const Input: FC<InputProps> = ({
-	className,
+	className = '',
 	leftIcon,
+	rightIcon,
 	...attributes
 }) => (
-	<div>
-		{leftIcon && <div />}
+	<div className='flex items-center px-2 py-2.5 max-w-sm rounded-xl'>
+		{leftIcon && <div className='flex'>{leftIcon}</div>}
 		<input
 			type='text'
-			className={`${className} px-2 py-2.5 rounded-xl placeholder-neutral-400`}
+			className={`text-smrounded-xl ml-[6px] mr-5 placeholder-neutral-400 bg-transparent ${className} flex-auto`}
 			{...attributes}
 		/>
+		{rightIcon && <div className='flex'>{rightIcon}</div>}
 	</div>
 )
