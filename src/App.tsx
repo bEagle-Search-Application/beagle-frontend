1853a30363342b9cd2b4511d48c4b7ae076ecd0b
Darwin_N19 <darwinccn25@gmail.com>
2022-07-24 19:18:01 -0500

    wip: input c

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Input } from './components'
+
 function App() {
 	return (
-		<div className='App text-center'>
+		<div className='App text-center bg-sky-400'>
 			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<Input placeholder='Inserta' />
 		</div>
 	)
 }
--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,3 +1,9 @@
-export function Input() {
-	return <div>Input</div>
+import { FC, InputHTMLAttributes } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	className?: string
 }
+
+export const Input: FC<InputProps> = ({ className, ...attributes }) => (
+	<input type='text' className={className} {...attributes} />
+)
