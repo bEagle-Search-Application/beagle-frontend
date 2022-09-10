fbba93bde743a104a48817e29eed20dbcbe032c2
Darwin_N19 <darwinccn25@gmail.com>
2022-09-09 22:32:01 -0500

    new Settings page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,83 @@
+import { LeftIcon, DownArrowIcon } from '../assets'
+import { Button, Input } from '../components'
+import { MainLayout } from '../layouts'
+
+export const Settings = () => (
+	<MainLayout>
+		<div className='ml-7 mt-10'>
+			<div className='max-w-xl'>
+				<div className='flex flex-col gap-10'>
+					<div>
+						<Button
+							size='small'
+							content='Volver'
+							leftIcon={<LeftIcon size={16} stroke='#4B5563' />}
+						/>
+					</div>
+					<div className=''>
+						<h2 className='text-neutral-900 text-4xl font-bold'>
+							Configuraciones
+						</h2>
+						<div className='mt-2 text-sm'>
+							<span className='text-neutral-400'>Configuraciones /</span>
+							<span className='text-neutral-600'> General</span>
+						</div>
+					</div>
+					<form>
+						<div className='flex flex-col gap-4'>
+							<div>
+								<span className='text-neutral-600 text-sm font-bold'>
+									Nombre de usuario
+								</span>
+								<Input
+									className='bg-neutral-100 w-full'
+									placeholder='pepeargento'
+								/>
+							</div>
+							<div>
+								<span className='text-neutral-600 text-sm font-bold'>
+									Email
+								</span>
+								<Input
+									className='bg-neutral-100'
+									placeholder='pepeargento@gmail.com'
+								/>
+							</div>
+							<div>
+								<span className='text-neutral-600 text-sm font-bold'>
+									Número de teléfono
+								</span>
+								<Input
+									className='border-[1px] border-neutral-300 border-solid'
+									leftIcon={
+										<div className='flex items-center gap-1'>
+											{/* TODO: Agregar las banderas dependiendo del país */}
+											🇲🇽
+											<DownArrowIcon size={12} stroke='#4B5563' />
+										</div>
+									}
+								/>
+							</div>
+						</div>
+
+						<div className='mt-10 flex justify-between items-center'>
+							<div>
+								{/* TODO: Preguntar el color del hover y active */}
+								<Button
+									size='small'
+									content='Volver'
+									leftIcon={<LeftIcon size={16} stroke='#4B5563' />}
+								/>
+							</div>
+							<Button
+								className='bg-primary-500 hover:bg-primary-700 active:bg-primary-900 text-white'
+								size='medium'
+								content='Enviar'
+							/>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</MainLayout>
+)
