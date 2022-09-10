41dc2c94a19465ea4083bc8140541c80b61c33b9
Darwin_N19 <darwinccn25@gmail.com>
2022-09-09 22:32:12 -0500

    Settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import { Dashboard, HelpPage, AboutTheObject, Objects } from './pages'
+import { Dashboard, HelpPage, AboutTheObject, Objects, Settings } from './pages'
 
 function App() {
 	return (
@@ -10,10 +10,12 @@ function App() {
 			<Route path='/informacion-del-objeto' element={<AboutTheObject />} />
 
 			{/* TODO: Dinamizar mis objetos perdidos/encontrados */}
+			{/* TODO: Se necesita una cuenta para ver estas páginas */}
 			<Route path='/mis-perdidos' element={<Objects />} />
 			<Route path='/mis-encontrados' element={<Objects />} />
 
 			<Route path='/ayuda' element={<HelpPage />} />
+			<Route path='/configuraciones' element={<Settings />} />
 
 			<Route path='*' element={<h1>Introduzca una ruta correcta</h1>} />
 		</Routes>
