bf62ccd0f47fc18ad3dde8d34158f19905694154
Darwin_N19 <darwinccn25@gmail.com>
2022-09-11 18:44:40 -0500

    feat: responsive media query for 1024px+

--- src/components/organisms/GridCard/GridCard.tsx
+++ src/components/organisms/GridCard/GridCard.tsx
@@ -1,8 +1,10 @@
 import { Card } from '../../molecules'
 
 export const GridCard = () => (
-	<div className='grid gap-4 lg:grid-cols-2 desktop:grid-cols-3'>
+	<div className='grid gap-4 lg:grid-cols-2 desktop:grid-cols-3 ultrawide:grid-cols-4'>
 		{/* TODO: Dinamizar el agrupamiento y usar algún "map" para mostrar las cards */}
+		<Card title='Cartera de cuero marrón rojizo con mucho valor dentro' />
+		<Card title='Cartera de cuero marrón rojizo con mucho valor dentro' />
 		<Card title='Cartera de cuero marrón rojizo con mucho valor dentro' />
 		<Card title='Cartera de cuero marrón rojizo con mucho valor dentro' />
 		<Card title='Cartera de cuero marrón rojizo con mucho valor dentro' />
