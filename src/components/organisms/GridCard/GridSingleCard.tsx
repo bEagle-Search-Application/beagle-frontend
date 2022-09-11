e6a8e7fbcee432d90bc57d3074b0855a35daf002
Darwin_N19 <darwinccn25@gmail.com>
2022-09-11 18:44:14 -0500

    feat: responsive for a media query 1024px´+

--- src/components/organisms/GridCard/GridSingleCard.tsx
+++ src/components/organisms/GridCard/GridSingleCard.tsx
@@ -1,7 +1,7 @@
 import { SingleCard } from '../../molecules'
 
 export const GridSingleCard = () => (
-	<div className='grid grid-cols-3 gap-5'>
+	<div className='grid gap-4 lg:grid-cols-2 desktop:grid-cols-3 ultrawide:grid-cols-4'>
 		{/* TODO: Dinamizar el agrupamiento y usar algún "map" para mostrar las cards */}
 		<SingleCard
 			title='Bolso Beige'
