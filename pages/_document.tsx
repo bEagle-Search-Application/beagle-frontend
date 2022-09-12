38076f58efe46b790017523ab890b4f695fb1003
Darwin_N19 <darwinccn25@gmail.com>
2022-09-12 17:17:44 -0500

    custom _document pag

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initalProps = await Document.getInitialProps(ctx)
+
+    return initalProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
