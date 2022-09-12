fd6ea92792561b98c14a7cccbd97e1073bfd7802
Darwin_N19 <darwinccn25@gmail.com>
2022-09-12 17:18:54 -0500

    fix: parent container of navbar

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -2,21 +2,21 @@ import { FC, ReactNode } from 'react'
 import { Navbar, Sidebar } from '../components'
 
 interface Props {
-	children: ReactNode
+  children: ReactNode
 }
 
 export const MainLayout: FC<Props> = ({ children }) => (
-	<div className='mx-auto'>
-		<Navbar />
-		<main className='flex'>
-			<div className='max-w-[280px]'>
-				<Sidebar />
-			</div>
-			{/* TODO: cambiar el text -left */}
-			{/* TODO: hacer responsive */}
-			<div className='w-full px-4 border-l border-solid border-neutral-300'>
-				{children}
-			</div>
-		</main>
-	</div>
+  <div className='mx-auto'>
+    <Navbar />
+    <main className='flex'>
+      <div className='max-w-[280px]'>
+        <Sidebar />
+      </div>
+      {/* TODO: cambiar el text -left */}
+      {/* TODO: hacer responsive */}
+      <div className='w-full px-4 border-l border-solid border-neutral-300'>
+        {children}
+      </div>
+    </main>
+  </div>
 )
