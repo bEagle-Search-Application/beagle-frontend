5a6a63a609e7112b9518eabc1747ae4c6c133662
Darwin_N19 <darwinccn25@gmail.com>
2022-09-15 19:55:45 -0500

    feat: remove margin right 20px (mr-5) property

--- components/atoms/Input/Input.tsx
+++ components/atoms/Input/Input.tsx
@@ -2,36 +2,36 @@ import { FC, InputHTMLAttributes, ReactElement } from 'react'
 import { IconProps } from '../../../interfaces'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-	className?: string
-	leftIcon?: ReactElement<IconProps>
-	rightIcon?: ReactElement<IconProps>
-	placeholderColor?: string
+  className?: string
+  leftIcon?: ReactElement<IconProps>
+  rightIcon?: ReactElement<IconProps>
+  placeholderColor?: string
 
-	labelInput?: ReactElement
+  labelInput?: ReactElement
 }
 
 export const Input: FC<InputProps> = ({
-	className = '',
-	leftIcon,
-	rightIcon,
-	placeholderColor = 'placeholder-neutral-400',
+  className = '',
+  leftIcon,
+  rightIcon,
+  placeholderColor = 'placeholder-neutral-400',
 
-	labelInput,
+  labelInput,
 
-	...attributes
+  ...attributes
 }) => (
-	<>
-		<div
-			className={`text-sm flex items-center px-2 py-2.5 rounded-xl ${className}`}
-		>
-			{leftIcon && <div className='flex'>{leftIcon}</div>}
-			<input
-				type='text'
-				className={`ml-[6px] mr-5 ${placeholderColor} bg-transparent flex-auto caret-primary-600`}
-				{...attributes}
-			/>
-			{rightIcon && <div className='flex'>{rightIcon}</div>}
-		</div>
-		{labelInput && labelInput}
-	</>
+  <>
+    <div
+      className={`text-sm flex items-center px-2 py-2.5 rounded-xl ${className}`}
+    >
+      {leftIcon && <div className='flex'>{leftIcon}</div>}
+      <input
+        type='text'
+        className={`ml-[6px] ${placeholderColor} bg-transparent flex-auto caret-primary-600`}
+        {...attributes}
+      />
+      {rightIcon && <div className='flex'>{rightIcon}</div>}
+    </div>
+    {labelInput && labelInput}
+  </>
 )
