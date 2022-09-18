141566014b2d3d09299abe584cc590dc93f89ba0
Darwin_N19 <darwinccn25@gmail.com>
2022-09-17 22:06:51 -0500

    feat: new handle modal

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,46 +1,65 @@
-import { FC, FormEvent, ReactNode, useContext, useState } from 'react'
+import { FC, ReactNode, useContext } from 'react'
+
 import {
-  CancelIcon,
-  FacebookCustomIcon,
-  FacebookIcon,
-  GoogleCustomIcon,
-  IdeaIcon,
-  SmileIcon,
-  SmileyFaceIcon,
-  TwitterCustomIcon,
-} from '../assets'
-import {
-  Button,
-  Input,
-  LabelInput,
+  AccountCreated,
   LogIn,
+  MessageCode,
   Navbar,
   Register,
   Sidebar,
 } from '../components'
 import { UIContext } from '../context'
+import { ITypeOfModals } from '../interfaces'
 
 interface Props {
   children: ReactNode
 }
 
-type ModalProps = 'login' | 'register'
-
 export const MainLayout: FC<Props> = ({ children }) => {
-  const { state, handleCloseModal } = useContext(UIContext)
-  const [whichModal, setWhichModal] = useState<ModalProps>('login')
+  const { state, handleOpenModal, handleCloseModal } = useContext(UIContext)
 
-  const handleModal = (modal: ModalProps) => {
-    setWhichModal(modal)
-  }
+  const RenderAccountModal = (modal: ITypeOfModals) => {
+    switch (modal) {
+      case 'login':
+        return (
+          <LogIn
+            handleCloseModal={handleCloseModal}
+            handleOpenModal={handleOpenModal}
+          />
+        )
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+      case 'register':
+        return (
+          <Register
+            handleCloseModal={handleCloseModal}
+            handleOpenModal={handleOpenModal}
+          />
+        )
+
+      case 'message-code':
+        return (
+          <MessageCode
+            handleCloseModal={handleCloseModal}
+            handleOpenModal={handleOpenModal}
+          />
+        )
+
+      case 'account-created':
+        return <AccountCreated handleCloseModal={handleCloseModal} />
+
+      default:
+        return (
+          <LogIn
+            handleCloseModal={handleCloseModal}
+            handleOpenModal={handleOpenModal}
+          />
+        )
+    }
   }
 
   return (
     <div className='mx-auto'>
-      <Navbar />
+      <Navbar handleOpenModal={handleOpenModal} />
       <main className='flex'>
         <div className='max-w-[280px]'>
           <Sidebar />
@@ -53,20 +72,10 @@ export const MainLayout: FC<Props> = ({ children }) => {
       </main>
       {/* MODAL */}
       {state.modalIsOpen && (
-        <div className='fixed top-0 w-full h-screen backdrop-blur bg-neutral-900/40'>
-          <div className='max-w-md bg-white relative mx-auto top-[50%] translate-y-[-50%] rounded-xl'>
-            {/* LOGIN */}
-            <LogIn
-              whichModal={whichModal}
-              handleModal={handleModal}
-              handleCloseModal={handleCloseModal}
-            />
-            {/* REGISTER */}
-            <Register
-              whichModal={whichModal}
-              handleModal={handleModal}
-              handleCloseModal={handleCloseModal}
-            />
+        <div className='fixed left-0 top-0 w-full h-full z-10 overflow-auto backdrop-blur bg-neutral-900/40'>
+          {/* <div className='max-w-md bg-white relative mx-auto top-[50%] translate-y-[-50%] rounded-xl'> */}
+          <div className='flex w-fit bg-white mx-auto relative top-[50%] translate-y-[-50%] rounded-xl'>
+            {RenderAccountModal(state.whichModal)}
           </div>
         </div>
       )}
