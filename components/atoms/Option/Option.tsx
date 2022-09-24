70b5322cf859232bbc3de1bf8b083599822c9eb2
Darwin_N19 <darwinccn25@gmail.com>
2022-09-23 19:48:03 -0500

    feat: label can be string or react element

--- components/atoms/Option/Option.tsx
+++ components/atoms/Option/Option.tsx
@@ -2,26 +2,26 @@ import { FC, HTMLAttributes, ReactElement } from 'react'
 import { IconProps } from '../../../interfaces'
 
 interface OptionProps extends HTMLAttributes<HTMLDivElement> {
-	label: string
+  label: string | ReactElement
 
-	leftIcon?: ReactElement<IconProps>
-	rightIcon?: ReactElement<IconProps>
+  leftIcon?: ReactElement<IconProps>
+  rightIcon?: ReactElement<IconProps>
 }
 
 export const Option: FC<OptionProps> = ({
-	label,
-	className,
-	leftIcon,
-	rightIcon,
+  label,
+  className,
+  leftIcon,
+  rightIcon,
 
-	...attributes
+  ...attributes
 }) => (
-	<div
-		className={`flex items-center gap-[10px] ${className || ''}`}
-		{...attributes}
-	>
-		{leftIcon && <div>{leftIcon}</div>}
-		<span>{label}</span>
-		{rightIcon && <div>{rightIcon}</div>}
-	</div>
+  <div
+    className={`flex items-center gap-[10px] ${className || ''}`}
+    {...attributes}
+  >
+    {leftIcon && <div>{leftIcon}</div>}
+    {label}
+    {rightIcon && <div>{rightIcon}</div>}
+  </div>
 )
