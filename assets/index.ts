c3cdadb6688cd160464ac41998afccd6134bc8e5
Darwin_N19 <darwinccn25@gmail.com>
2022-09-24 23:53:35 -0500

    update imports

--- assets/index.ts
+++ assets/index.ts
@@ -40,6 +40,7 @@ export { RightArrowIcon } from './icons/RightArrowIcon'
 export { ExitAppIcon } from './icons/ExitAppIcon'
 export { GiveHelpIcon } from './icons/GiveHelpIcon'
 export { WarningIcon } from './icons/WarningIcon'
+export { AnnonationQuestionIcon } from './icons/AnnonationQuestionIcon'
 
 export * from './icons/custom'
 // TODO: Preguntarle a Isaias si pudiese hacer que estos 2 íconos no vengan en grupo
