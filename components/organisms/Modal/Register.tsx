3dfaf70993a80d84fff48c61e290ac613683a8c0
Darwin_N19 <darwinccn25@gmail.com>
2022-10-10 21:31:26 -0500

    Formik and Yup were integrated with user registration

--- components/organisms/Modal/Register.tsx
+++ components/organisms/Modal/Register.tsx
@@ -1,4 +1,7 @@
-import { FC, FormEvent, useContext, useState } from 'react'
+import { FC, useContext } from 'react'
+
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 import { Input, LabelInput, Button } from '../../atoms'
 import {
@@ -8,9 +11,11 @@ import {
   GoogleCustomIcon,
   FacebookIcon,
   TwitterCustomIcon,
+  SuccessCheckIcon,
+  AddCircleIcon,
 } from '../../../assets'
-import { useForm } from '../../../hooks'
 import { AuthContext, UIContext } from '../../../context'
+import { regExp } from '../../../utils'
 
 const INITIAL_VALUES_REGISTER_FORM = {
   name: '',
@@ -18,35 +23,66 @@ const INITIAL_VALUES_REGISTER_FORM = {
   email: '',
   password: '',
   phone: 0,
+  termsAndConditions: false,
+  allowNotifications: false,
 }
 
 export const Register: FC = () => {
-  const [buttonDisabled, setButtonDisabled] = useState(false)
   const { handleOpenModal, handleCloseModal } = useContext(UIContext)
   const { createUser } = useContext(AuthContext)
-  const { formValues, handleChange } = useForm(INITIAL_VALUES_REGISTER_FORM)
-  const { name, surname, email, password, phone } = formValues
+  const {
+    getFieldProps,
+    handleSubmit,
+    touched,
+    errors,
+    isSubmitting,
+    isValid,
+  } = useFormik({
+    initialValues: INITIAL_VALUES_REGISTER_FORM,
+    onSubmit: async (values) => {
+      const { name, surname, email, password, phone } = values
+      const isRegisterValid = await createUser(
+        name.trim(),
+        surname.trim(),
+        email.trim(),
+        password.trim(),
+        Number(phone.toString().trim())
+      )
 
-  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setButtonDisabled(false)
+      if (!isRegisterValid) {
+        alert('Error al crear usuario')
+        return
+      }
 
-    const isRegisterValid = await createUser(
-      name,
-      surname,
-      email,
-      password,
-      phone
-    )
-    if (isRegisterValid) {
       handleCloseModal()
       return handleOpenModal('account-created')
-    } else {
-      alert('Error al crear usuario')
-      setButtonDisabled(true)
-      return
-    }
-  }
+    },
+    //TODO: Verificar si la validación es la correcta y mejorarla
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required('El nombre es requerido')
+        .min(3, 'El nombre debe tener al menos 3 caracteres')
+        .matches(regExp.name, 'Introduzca un nombre válido'),
+      surname: Yup.string()
+        .required('El apellido es requerido')
+        .min(3, 'El apellido debe tener al menos 3 caracteres')
+        .matches(regExp.name, 'Introduzca un apellido válido'),
+      email: Yup.string()
+        .email('Introduzca un email válido')
+        .required('El email es requerido'),
+      password: Yup.string().required('La contraseña es requerida'),
+      phone: Yup.number()
+        .test(
+          'len',
+          'El número debe tener al menos 6 dígitos',
+          (val = 0) => val.toString().length >= 6
+        )
+        .required('El teléfono es requerido'),
+      termsAndConditions: Yup.boolean()
+        .oneOf([true], 'Debe aceptar los términos y condiciones')
+        .required(),
+    }),
+  })
 
   return (
     <div className='w-[27.4375rem]'>
@@ -65,27 +101,45 @@ export const Register: FC = () => {
             Bienvenido a bEagle!
           </h5>
         </div>
-        <form onSubmit={handleFormSubmit} className='w-full px-8 text-sm'>
+
+        {/* Form Register */}
+        <form
+          className='w-full px-8 text-sm'
+          noValidate
+          onSubmit={handleSubmit}
+        >
           <div className='flex gap-4 box-content'>
             <div className='flex-1 flex-col gap-1'>
               <span className='text-neutral-600 font-bold'>Nombre</span>
               <Input
                 className='bg-neutral-100'
                 placeholder='Escribe tu nombre aquí'
-                name='name'
-                onChange={handleChange}
-                value={name}
+                id='name'
+                {...getFieldProps('name')}
               />
+              {touched['name'] && errors.name && (
+                <LabelInput
+                  type='error'
+                  text={errors.name}
+                  icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+                />
+              )}
             </div>
             <div className='flex-1 flex-col gap-1'>
               <span className='text-neutral-600 font-bold'>Apellido</span>
               <Input
                 className='bg-neutral-100'
                 placeholder='Escribe tu apellido aquí'
-                name='surname'
-                onChange={handleChange}
-                value={surname}
+                id='surname'
+                {...getFieldProps('surname')}
               />
+              {touched['surname'] && errors.surname && (
+                <LabelInput
+                  type='error'
+                  text={errors.surname}
+                  icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+                />
+              )}
             </div>
           </div>
           <div className='mt-4 flex flex-col gap-1'>
@@ -93,10 +147,16 @@ export const Register: FC = () => {
             <Input
               className='bg-neutral-100'
               placeholder='Escribe tu email aquí'
-              name='email'
-              onChange={handleChange}
-              value={email}
+              id='email'
+              {...getFieldProps('email')}
             />
+            {touched['email'] && errors.email && (
+              <LabelInput
+                type='error'
+                text={errors.email}
+                icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+              />
+            )}
           </div>
           <div className='mt-4 flex flex-col gap-1'>
             <span className='text-neutral-600 font-bold'>Contraseña</span>
@@ -104,14 +164,20 @@ export const Register: FC = () => {
               type='password'
               className='bg-neutral-100'
               placeholder='Escribe tu contraseña aquí'
-              name='password'
-              onChange={handleChange}
-              value={password}
+              id='password'
+              {...getFieldProps('password')}
             />
+            {touched.password && errors.password && (
+              <LabelInput
+                type='error'
+                text={errors.password}
+                icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+              />
+            )}
             <LabelInput
               type='primary'
               icon={<IdeaIcon size={20} stroke='#655BA4' />}
-              text='Recuerda que tu contraseña debe contener al menos una mayúscula, un número, y un cáracter especial (ej. $%^$@!#). '
+              text='Recuerda que tu contraseña debe contener al menos una mayúscula, un número, y un cáracter especial (ej. $%^$@!#).'
             />
           </div>
           <div className='mt-4 flex flex-col gap-1'>
@@ -122,14 +188,25 @@ export const Register: FC = () => {
               type='number'
               className='bg-neutral-100'
               placeholder='Escribe tu número de teléfono aquí'
-              name='phone'
-              onChange={handleChange}
-              value={phone}
+              id='phone'
+              {...getFieldProps('phone')}
             />
+            {touched.phone && errors.phone && (
+              <LabelInput
+                type='error'
+                text={errors.phone}
+                icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+              />
+            )}
           </div>
+          {/* Ver si validaremos celular */}
           <div className='mt-4 flex flex-col gap-3 text-xs text-neutral-600'>
             <label className='w-fit flex items-center gap-1'>
-              <input type='checkbox' />
+              <input
+                type='checkbox'
+                id='termsAndConditions'
+                {...getFieldProps('termsAndConditions')}
+              />
               <span>
                 Acepto los{' '}
                 <span className='underline'>términos y condiciones</span> de
@@ -137,7 +214,12 @@ export const Register: FC = () => {
               </span>
             </label>
             <label className='w-fit flex items-center gap-1'>
-              <input type='checkbox' />
+              <input
+                type='checkbox'
+                id='allowNotifications'
+                {...getFieldProps('allowNotifications')}
+              />
+              {/* Preguntar si es 'No deseo' o 'deseo' */}
               <span>
                 No deseo recibir emails, promociones, beneficios, o NFTs.
               </span>
@@ -148,8 +230,8 @@ export const Register: FC = () => {
               type='submit'
               size='medium'
               content='Continuar'
-              className='w-full justify-center text-white bg-primary-500 hover:bg-primary-700 active:bg-primary-900'
-              disabled={buttonDisabled}
+              className='w-full justify-center text-white bg-primary-500 hover:bg-primary-700 active:bg-primary-900  disabled:opacity-50'
+              disabled={!isValid}
             />
             {/* TODO: Hover y active */}
             <Button
@@ -160,7 +242,10 @@ export const Register: FC = () => {
             />
           </div>
         </form>
+
         <hr className='mx-8 h-[1px] border-t border-neutral-300' />
+
+        {/* Social media */}
         <div className='mx-8 flex items-center flex-col gap-4'>
           <span className='text-neutral-600'>Registrarse con</span>
           <div className='flex gap-8 justify-center items-center'>
@@ -170,6 +255,14 @@ export const Register: FC = () => {
           </div>
         </div>
       </div>
+      {isSubmitting && (
+        <div className='w-full h-full absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] flex items-center justify-center backdrop-blur-[1px] bg-neutral-500/20 z-50 rounded-xl'>
+          <div className='animate-spin'>
+            {/* TODO: Cambiar de ícono por un spinner */}
+            <AddCircleIcon size={50} stroke='#000' />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
