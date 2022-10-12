20d27e50ee2da0dcf47086578c5f25ea7ef45e65
Darwin_N19 <darwinccn25@gmail.com>
2022-10-11 20:25:56 -0500

    integrated with formik and yup validator

--- components/organisms/Modal/LogIn.tsx
+++ components/organisms/Modal/LogIn.tsx
@@ -1,18 +1,20 @@
-import { FC, FormEvent, useContext, useState } from 'react'
+import { FC, useContext, useState } from 'react'
 import { useRouter } from 'next/router'
 
-import { Input, Button } from '../../atoms'
+import { Input, Button, LabelInput } from '../../atoms'
 import {
   CancelIcon,
   FacebookIcon,
   GoogleCustomIcon,
-  SmileIcon,
+  HappyFaceIcon,
+  SuccessCheckIcon,
   TwitterCustomIcon,
 } from '../../../assets'
-import { useForm } from '../../../hooks'
 import { AuthContext, UIContext } from '../../../context'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
-const INITIAL_VALUES_REGISTER_FORM = {
+const INITIAL_VALUES_LOGIN_FORM = {
   email: '',
   password: '',
 }
@@ -21,30 +23,38 @@ export const LogIn: FC = () => {
   const router = useRouter()
   const { loginUser } = useContext(AuthContext)
   const { handleOpenModal, handleCloseModal } = useContext(UIContext)
-  const [buttonDisabled, setButtonDisabled] = useState(false)
-  const { formValues, handleChange } = useForm(INITIAL_VALUES_REGISTER_FORM)
-  const { email, password } = formValues
+  const [submitStateForm, setSubmitStateForm] = useState({
+    isSubmitValid: false,
+    messageError: '',
+  })
+  const {
+    touched,
+    errors,
+    handleSubmit,
+    getFieldProps,
+    isValid,
+    isSubmitting,
+  } = useFormik({
+    initialValues: INITIAL_VALUES_LOGIN_FORM,
+    onSubmit: async (values) => {
+      const isValidLogin = await loginUser(values.email, values.password)
+      const { redirect = '' } = router.query
 
-  // TODO: Validar campos del formulario
-  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setButtonDisabled(true)
+      if (!isValidLogin) {
+        return setSubmitStateForm({
+          isSubmitValid: false,
+          messageError: 'Usuario o contraseña incorrecto',
+        })
+      }
 
-    const isValidLogin = await loginUser(email, password)
-    const { redirect = '' } = router.query
-
-    if (!isValidLogin) {
-      //TODO: Manejo de errores
-      setButtonDisabled(false)
-      return alert('Usuario o contraseña incorrectos')
-    }
-
-    handleCloseModal()
-
-    if (!!redirect) {
-      return router.push(redirect as string)
-    }
-  }
+      handleCloseModal()
+      if (!!redirect) return router.push(redirect as string)
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Email inválido').required('Email requerido'),
+      password: Yup.string().required('Contraseña requerida'),
+    }),
+  })
 
   return (
     <div className='w-[27.5rem] max-w-md'>
@@ -55,53 +65,71 @@ export const LogIn: FC = () => {
       </div>
       <div className='flex flex-col gap-8 pb-10'>
         <div className='flex flex-col items-center gap-4'>
-          {/* TODO: Usar el ícono que esta en figma */}
           <div className='text-center'>
-            <SmileIcon size={24} stroke='#4B5563' />
+            <HappyFaceIcon size={24} stroke='#4B5563' />
           </div>
           <h5 className='text-neutral-900 text-[1.5rem] leading-7 font-bold'>
             Que bueno tenerte de vuelta!
           </h5>
         </div>
-        <form onSubmit={handleFormSubmit} className='w-full px-8 text-sm'>
+        <form onSubmit={handleSubmit} className='w-full px-8 text-sm'>
           <div className='flex flex-col gap-1'>
             <span className='text-neutral-600 font-bold'>Email</span>
             <Input
               className='bg-neutral-100'
               placeholder='Escribe tu email aquí'
-              name='email'
-              value={email}
-              onChange={handleChange}
+              id='email'
+              {...getFieldProps('email')}
             />
+            {touched.email && errors.email && (
+              <LabelInput
+                type='error'
+                text={errors.email}
+                icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+              />
+            )}
           </div>
           <div className='mt-4 flex flex-col gap-1'>
             <span className='text-neutral-600 font-bold'>Contraseña</span>
             <Input
               type='password'
               className='bg-neutral-100'
-              name='password'
-              value={password}
-              onChange={handleChange}
+              id='password'
+              {...getFieldProps('password')}
             />
+            {touched.password && errors.password && (
+              <LabelInput
+                type='error'
+                text={errors.password}
+                icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+              />
+            )}
             <span className='mx-3 my-[6px] text-primary-500 text-xs'>
               ¿Olvidaste tu contraseña?
             </span>
           </div>
-          {/* TODO: Button iniciar sesion */}
+          <div>
+            {submitStateForm.messageError.length !== 0 && (
+              <LabelInput
+                type='error'
+                text={submitStateForm.messageError}
+                icon={<SuccessCheckIcon size={12} stroke='#B91C1C' />}
+              />
+            )}
+          </div>
           <div className='mt-8 flex flex-col gap-3'>
             <Button
               type='submit'
               size='medium'
               content='Iniciar sesión'
-              className='w-full justify-center text-white bg-primary-500 hover:bg-primary-700 active:bg-primary-900'
-              disabled={buttonDisabled}
+              className='w-full justify-center text-white bg-primary-500 hover:bg-primary-700 active:bg-primary-900 disabled:opacity-50'
+              disabled={!isValid || isSubmitting}
             />
-            {/* TODO: Hover y active */}
             <Button
               size='medium'
               content='Crear cuenta'
               onClick={() => handleOpenModal('register')}
-              className='w-full justify-center text-neutral-600 hover:bg-neutral-100 active:bg-neutral-300'
+              className='w-full justify-center text-neutral-600 hover:text-neutral-700 active:text-neutral-800'
             />
           </div>
         </form>
