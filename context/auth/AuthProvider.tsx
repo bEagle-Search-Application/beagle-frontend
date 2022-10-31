596d90c8453dcd4ea17c4854824d5f4126b3a6df
Darwin_N19 <darwinccn25@gmail.com>
2022-10-30 21:55:17 -0500

    feat: Expiration time for tokens and documentation of all functions

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -24,21 +24,6 @@ export const AuthProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE)
   const router = useRouter()
 
-  // useEffect(() => {
-  //   const token = Cookies.get('token')
-  //   const email = localStorage.getItem('email')
-  //   if (!token || !email) return
-
-  //   bEagleApi
-  //     .post(`/api/users/verify/${token}`)
-  //     .then((res) => {
-  //       console.log(res)
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     })
-  // }, [])
-
   useEffect(() => {
     checkToken()
   }, [])
@@ -55,23 +40,37 @@ export const AuthProvider: FC<Props> = ({ children }) => {
     })
   }
 
+  /**
+   *
+   * Función que se encarga de logear al usuario
+   * En localStorage se almacena el usuario
+   * En cookies se almacena el token de acceso(expira en 10min) y el token de refresco(expira en 10d)
+   *
+   * @param email
+   * @param password
+   * @returns true si el usuario se logeo correctamente, false en caso contrario
+   */
   const loginUser = async (
     email: string,
     password: string
   ): Promise<boolean> => {
     try {
-      const { data } = await bEagleApi.get('/auth/login', {
-        params: {
-          email,
-          password,
-        },
+      const { data } = await bEagleApi.post('/auth/login', {
+        email,
+        password,
       })
       const { response } = await data
       const { user, auth } = (await response) as ILoginResponse
       dispatch({ type: '[AUTH] - LOGIN', payload: user })
       window.localStorage.setItem('user-data', JSON.stringify(user))
-      Cookies.set('access_token', auth.access_token)
-      Cookies.set('refresh_token', auth.refresh_token)
+      Cookies.set('access_token', auth.access_token, {
+        expires: 1 / 144,
+        path: '/',
+      })
+      Cookies.set('refresh_token', auth.refresh_token, {
+        expires: 15,
+        path: '/',
+      })
 
       // TODO: Necesitaría una función que me permita saber si el token sigue funcionando, cada que el usuario refresque la página
 
@@ -81,14 +80,30 @@ export const AuthProvider: FC<Props> = ({ children }) => {
     }
   }
 
+  /**
+   * Función que se encarga de cerrar la sesión del usuario
+   * Remueve el usuario del localStorage y los tokens de las cookies
+   */
   const logoutUser = () => {
     dispatch({ type: '[AUTH] - LOGOUT' })
+    bEagleApi.post('/logout', {
+      token: Cookies.get('refresh_token'),
+    })
     router.push('/')
     window.localStorage.removeItem('user-data')
     Cookies.remove('access_token')
     Cookies.remove('refresh_token')
   }
 
+  /**
+   *
+   * @param name
+   * @param surname
+   * @param email
+   * @param password
+   * @param phone
+   * @returns true si el usuario se registro correctamente, false en caso contrario
+   */
   const createUser = async (
     name: string,
     surname: string,
@@ -104,7 +119,7 @@ export const AuthProvider: FC<Props> = ({ children }) => {
       phone_prefix: phone.toString().slice(0, 2),
       phone: phone.toString().slice(2, -1),
     })
-    if (response.status === 204) return true
+    if (response.status === 201) return true
     return false
   }
 
