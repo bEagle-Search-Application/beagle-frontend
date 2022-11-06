935f94b0d5f59338df098e35c037e6dec9b8c110
Darwin_N19 <darwinccn25@gmail.com>
2022-11-06 16:14:19 -0500

    feat: request is sent by the client

--- pages/email-confirmation/index.tsx
+++ pages/email-confirmation/index.tsx
@@ -1,61 +1,47 @@
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import { bEagleApi } from '../../api'
+import { AxiosError } from 'axios'
 
-const EmailConfirmationPage = ({ message }: { message: string }) => {
-  const [showMessage, setShowMessage] = useState(false)
+interface PropsError {
+  response: string
+}
+
+const EmailConfirmationPage = () => {
+  const [message, setMessage] = useState<string>('Verificando...')
+  const router = useRouter()
+  const { token = '' } = router.query
+
   useEffect(() => {
-    setShowMessage(true)
-  }, [])
+    const controller = new AbortController()
+    if (token === '') return
+    if (token.length < 15)
+      return setMessage('Asegúrese de enviar un token válido')
+
+    bEagleApi
+      .post('/users/verify/' + token, null, {
+        signal: controller.signal,
+      })
+      .then(() => {
+        setMessage('Email verificado correctamente')
+        setTimeout(() => {
+          router.push('/')
+        }, 2000)
+      })
+      .catch((err: AxiosError<PropsError>) => {
+        setMessage(err.response?.data.response || 'Error al verificar email')
+      })
+
+    return () => {
+      controller.abort()
+    }
+  }, [token, router])
 
   return (
     <div className='w-full h-screen flex justify-center items-center text-2xl'>
-      {showMessage ? message : 'Verificando...'}
+      {message}
     </div>
   )
-}
-
-// You should use getServerSideProps when:
-// - Only if you need to pre-render a page whose data must be fetched at request time
-import { GetServerSideProps } from 'next'
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { token = '' } = ctx.query
-
-  if (!token) {
-    return {
-      props: {
-        message: 'No se encontró el token o el email',
-      },
-    }
-  }
-
-  // TODO: Si la cuenta ya ha sido creada revisar cómo podriamos manejarlo
-  let response
-  try {
-    response = await bEagleApi.post('/users/verify/' + token, null, {
-      params: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    if (response.status === 204)
-      return {
-        props: {
-          message: 'Cuenta creada con éxito',
-        },
-      }
-  } catch (error) {
-    return {
-      props: {
-        message: 'Ha ocurrido un error al verificar el email o el token',
-      },
-    }
-  }
-
-  return {
-    props: {
-      message: 'Ha ocurrido un error al verificar el email o el token',
-    },
-  }
 }
 
 export default EmailConfirmationPage
