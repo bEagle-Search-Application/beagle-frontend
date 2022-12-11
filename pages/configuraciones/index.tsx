c94d3959f064e71a4b214444267984b39658dce0
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-12-11 19:34:56 -0300

    wip: Settings page OK but need some editions yet

--- pages/configuraciones/index.tsx
+++ pages/configuraciones/index.tsx
@@ -1,10 +1,10 @@
 import { LeftIcon, DownArrowIcon } from '../../assets'
-import { Button, Input, wrapperPrivateRoute } from '../../components'
+import { Button, Input, wrapperPrivateRoute, TextArea } from '../../components'
 import { MainLayout } from '../../layouts'
 
 const SettingsPage = () => (
   <MainLayout isSidebarMain={false}>
-    <div className='ml-7 mt-10'>
+    <div className='ml-7 mt-10 mb-10'>
       <div className='max-w-xl'>
         <div className='flex flex-col gap-10'>
           <div>
@@ -23,15 +23,48 @@ const SettingsPage = () => (
               <span className='text-neutral-600'> Editar perfil</span>
             </div>
           </div>
+          {/* Upload Photo */}
+          <div className='flex items-center gap-10'>
+            <div className='relative'>
+              <img
+                className='w-[120px] h-[120px] rounded-full object-cover'
+                src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
+                alt='Persona que lo encontró'
+              />
+              <div className='absolute w-7 h-7 rounded-full bg-red-500 bottom-1 right-2 drop-shadow-xl shadow-black'></div>
+            </div>
+            <Button
+              className='text-white bg-primary-500 hover:bg-primary-700 active:bg-primary-900'
+              size='medium'
+              content='Subir una foto'
+            />
+            <Button
+              className='text-red-500 active:bg-primary-900 -ml-5 hover:text-red-700'
+              size='medium'
+              content='Eliminar'
+            />
+          </div>
           <form>
+            <h3 className='uppercase text-xl mb-3 text-neutral-600'>
+              Información basica
+            </h3>
             <div className='flex flex-col gap-4'>
               <div>
                 <span className='text-neutral-600 text-sm font-bold'>
-                  Nombre de usuario
+                  Nombre
                 </span>
                 <Input
                   className='bg-neutral-100 w-full'
-                  placeholder='pepeargento'
+                  placeholder='Escribe tu Nombre'
+                />
+              </div>
+              <div>
+                <span className='text-neutral-600 text-sm font-bold'>
+                  Apellido
+                </span>
+                <Input
+                  className='bg-neutral-100'
+                  placeholder='Escribe tu Apellido'
                 />
               </div>
               <div>
@@ -58,6 +91,34 @@ const SettingsPage = () => (
                   }
                 />
               </div>
+              <div>
+                <span className='text-neutral-600 text-sm font-bold uppercase'>
+                  Bio
+                </span>
+                <TextArea
+                  className='bg-neutral-100'
+                  placeholder='Escribe una descripción que te ayude a ganar la confianza de los demás.'
+                />
+              </div>
+              <div>
+                <span className='text-neutral-600 text-sm font-bold uppercase'>
+                  Reseñas
+                </span>
+                <div className='flex items-center gap-4'>
+                  <span className='text-neutral-800 text-sm font-bold'>
+                    Mostrar reseñas en perfil
+                  </span>
+                  <label className='inline-flex relative items-center mr-5 cursor-pointer'>
+                    <input
+                      type='checkbox'
+                      value=''
+                      className='sr-only peer'
+                      checked
+                    />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-500"></div>
+                  </label>
+                </div>
+              </div>
             </div>
             <div className='mt-10 flex justify-between items-center'>
               <div className='text-neutral-600 hover:text-neutral-700 active:text-neutral-800'>
@@ -70,7 +131,7 @@ const SettingsPage = () => (
               <Button
                 className='text-white bg-primary-500 hover:bg-primary-700 active:bg-primary-900'
                 size='medium'
-                content='Enviar'
+                content='Guardar Cambios'
               />
             </div>
           </form>
