8eaee10119715e50ad00bd66addfb1872ac5a066
Darwin_N19 <darwinccn25@gmail.com>
2022-12-11 19:33:13 -0500

    feat: placeholderColor rename for textareaClassname

--- components/atoms/TextArea/TextArea.tsx
+++ components/atoms/TextArea/TextArea.tsx
@@ -2,14 +2,14 @@ import { FC, ReactElement, TextareaHTMLAttributes } from 'react'
 
 interface InputProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   className?: string
-  placeholderColor?: string
+  textareaClassName?: string
 
   labelInput?: ReactElement
 }
 
 export const TextArea: FC<InputProps> = ({
   className = '',
-  placeholderColor = 'placeholder-neutral-400',
+  textareaClassName = 'placeholder-neutral-400',
 
   labelInput,
 
@@ -20,9 +20,9 @@ export const TextArea: FC<InputProps> = ({
       className={`text-sm flex items-center px-2 py-2.5 rounded-xl ${className}`}
     >
       <textarea
-        className={`ml-[0.375rem] ${placeholderColor} bg-transparent flex-auto caret-primary-600 h-28 focus:outline-none`}
+        className={`ml-[0.375rem] ${textareaClassName} bg-transparent flex-auto caret-primary-600 h-28 focus:outline-none`}
         {...attributes}
-      ></textarea>
+      />
     </div>
     {labelInput && labelInput}
   </>
